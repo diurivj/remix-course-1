bf4789408353779ab2c26e1036cf8e5c113d595d
Diego Vazquez <diurivj@gmail.com>
2023-06-21 18:14:57 -0600

    Before starting day 3

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,31 +1,7 @@
-import { json, type LoaderArgs } from '@remix-run/node';
-import { Form, useLoaderData } from '@remix-run/react';
-
-// Loader -> Se ejecuta en el server.
-// Loader -> Lo primero que pasa antes de mostrar el UI.
-export function loader(_: LoaderArgs) {
-  const fakeData = {
-    msg: 'Holaaaaaaaa!',
-    date: new Date(),
-  };
-
-  return json(fakeData);
-}
-
-export function action() {
-  console.log('action');
-  return null;
-}
-
 export default function Index() {
-  const { msg } = useLoaderData<typeof loader>();
-
   return (
     <>
-      <h1 className='underline'>{msg}</h1>
-      <Form method='post'>
-        <button type='submit'>Request!</button>
-      </Form>
+      <h1>Hola!</h1>
     </>
   );
 }
